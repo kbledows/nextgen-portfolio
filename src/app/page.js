557ca8f70416170c828054948a23cfb9222bbf9b
Konrad Bledowski <32736495+kbledows@users.noreply.github.com>
2023-09-12 14:24:44 -0400

    make kbd larger

--- src/app/page.js
+++ src/app/page.js
@@ -22,7 +22,7 @@ export default function Home() {
           <div className="hero-content flex-col lg:flex-row-reverse">
             <Image src={pokemon} className="w-56 md:w-72 lg:w-82 rounded-lg" />
             <div>
-              <h1 className="text-xl md:text-4xl lg:text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-br from-blue-800 to-green-800 pb-1 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">Coding for Fun</h1>
+              <h1 className="text-xl md:text-4xl lg:text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-br from-blue-800 to-green-800 pb-2 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">Coding for Fun</h1>
               <p className="py-6">Please enjoy a game of Konordle ! I am an avid fan of video games, and an obsession with the famous Wordle has led me to create my own version. Play it by clicking the button below!</p>
               <Link href="/wordle"><button className="btn bg-[#344E41] text-[#DAD7CD] hover:text-[#344E41] hover:border-[#588157] hover:border-2 hover:bg-[#dad7cd]">Konordle</button></Link>
             </div>
--- src/components/Qwerty.js
+++ src/components/Qwerty.js
@@ -1,21 +1,21 @@
 import { observer } from 'mobx-react-lite'
 
-export default observer(function Qwerty({currentGuess, guess, word}){
-    const rowOne = ['q','w','e','r','t','y','u','i','o','p'];
-    const rowTwo = ['a','s','d','f','g','h','j','k','l'];
-    const rowThree = ['z','x','c','v','b','n','m'];
+export default observer(function Qwerty({ currentGuess, guess, word }) {
+    const rowOne = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'];
+    const rowTwo = ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'];
+    const rowThree = ['z', 'x', 'c', 'v', 'b', 'n', 'm'];
 
     let doneWords = [];
-    if (currentGuess > 0){
-        doneWords = guess.slice(0,currentGuess);
+    if (currentGuess > 0) {
+        doneWords = guess.slice(0, currentGuess);
     };
     function getColor(letter) {
-        if (doneWords.length > 0){
-            for (let i = 0; i < doneWords.length; i++){
-                if (doneWords[i].includes(letter)){
-                    if (word.includes(letter)){
-                        for (let j = 0; j < doneWords[i].length; j++){
-                            if (word[j] === doneWords[i][j]){
+        if (doneWords.length > 0) {
+            for (let i = 0; i < doneWords.length; i++) {
+                if (doneWords[i].includes(letter)) {
+                    if (word.includes(letter)) {
+                        for (let j = 0; j < doneWords[i].length; j++) {
+                            if (word[j] === doneWords[i][j]) {
                                 return "bg-[#6ca965]"; //green
                             }
                         }
@@ -26,44 +26,44 @@ export default observer(function Qwerty({currentGuess, guess, word}){
                     }
                 }
             }
-            
+
         }
         return "bg-[#dad7cd]";
-      };
+    };
     return (
         <>
-        <div className="h-4 text-black mt-6 lg:mt-1">
-            <div className={"flex justify-center  lg:gap-1 lg:my-1 w-full "}>
-                {new Array(rowOne.length).fill(0).map((_,i) => {
-                return (
-                    <div key = {i} >
-                        <kbd className={`kbd kbd-md lg:kbd-md ${getColor(rowOne[i])}`}>{rowOne[i]}</kbd>
-                    </div>
-                    
-                )  
-            })}
-            </div> 
-            <div className={"flex justify-center lg:gap-1 lg:my-1 w-full"}>
-                {new Array(rowTwo.length).fill(0).map((_,i) => {
-                return (
-                    <div key = {i} >
-                        <kbd className={`kbd kbd-md lg:kbd-md ${getColor(rowTwo[i])}`}>{rowTwo[i]}</kbd>
-                    </div>
-                    
-                )  
-            })}
-            </div> 
-            <div className={"flex justify-center lg:gap-1 lg:my-1 w-full"}>
-                {new Array(rowThree.length).fill(0).map((_,i) => {
-                return (
-                    <div key = {i} >
-                        <kbd className={`kbd kbd-md lg:kbd-md ${getColor(rowThree[i])}`}>{rowThree[i]}</kbd>
-                    </div>
-                    
-                )  
-            })}
-            </div> 
-        </div>
+            <div className="h-4 text-black mt-6 lg:mt-1">
+                <div className={"flex justify-center  lg:gap-1 lg:my-1 w-full "}>
+                    {new Array(rowOne.length).fill(0).map((_, i) => {
+                        return (
+                            <div key={i} >
+                                <kbd className={`kbd kbd-md lg:kbd-lg ${getColor(rowOne[i])}`}>{rowOne[i]}</kbd>
+                            </div>
+
+                        )
+                    })}
+                </div>
+                <div className={"flex justify-center lg:gap-1 lg:my-1 w-full"}>
+                    {new Array(rowTwo.length).fill(0).map((_, i) => {
+                        return (
+                            <div key={i} >
+                                <kbd className={`kbd kbd-md lg:kbd-lg ${getColor(rowTwo[i])}`}>{rowTwo[i]}</kbd>
+                            </div>
+
+                        )
+                    })}
+                </div>
+                <div className={"flex justify-center lg:gap-1 lg:my-1 w-full"}>
+                    {new Array(rowThree.length).fill(0).map((_, i) => {
+                        return (
+                            <div key={i} >
+                                <kbd className={`kbd kbd-md lg:kbd-lg ${getColor(rowThree[i])}`}>{rowThree[i]}</kbd>
+                            </div>
+
+                        )
+                    })}
+                </div>
+            </div>
         </>
     )
 })
