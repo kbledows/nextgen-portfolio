1db388c5cea38e4bce24126ea52041bc7ebf8f38
Konrad <32736495+kbledows@users.noreply.github.com>
2023-09-13 18:22:43 -0400

    Add analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import Footer from '@/components/Footer'
 import './globals.css'
 import Navbar from '@/components/Navbar'
-import Analytics from '@vercel/analytics/react'
+import { Analytics } from '@vercel/analytics/react'
 
 export const metadata = {
   title: 'Konrad Bledowski',
@@ -11,7 +11,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body> <Navbar />{children}<Footer /><Analytics /> </body>
+      <body> <Navbar />{children}<Footer /></body>
+      <Analytics />
     </html>
+
   )
 }
